33784aaa2230c6b466e1f2027a2cef4101695ef4
nadimahmed1 <nadimarafat26@gmail.com>
2023-01-23 17:24:05 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import bacGround from './images/backGround.jpg'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Pages/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+
 
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/home' element={<Home></Home>}></Route>
       </Routes>
 
     </div>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,51 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Carousel } from 'react-bootstrap';
+import banner1 from '../../images/banner/9009900_4047751.jpg'
+import banner2 from '../../images/banner/25797899_7129144.jpg'
+import banner3 from '../../images/banner/25232720_7046202.jpg'
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1 className='text-success'>Welcome to our BookStore</h1>
-        </div>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="banner-1 d-block w-100"
+                    src={banner1}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="banner-2 d-block w-100"
+                    src={banner2}
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="banner-3 d-block w-100"
+                    src={banner3}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 };
 
